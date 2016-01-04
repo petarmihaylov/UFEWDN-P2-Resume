dc825c7cbaa66f0dfa0da7ef58f1a50117dc3fcd
Petar Mihaylov <petar.m@studio350.com>
2016-01-04 04:55:55 -0500

    Replaced all "" with '', added gh-pages deploy support with Grunt

---
+++ Gruntfile.js
@@ -0,0 +1,14 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    'gh-pages': {
+    src: ['css/*.css', 'images/**/*', 'js/*.js', 'index.html']
+    //src: ['**'] //mathces all files in the cwd (current working directory)()
+  }
+  });
+
+  grunt.loadNpmTasks('grunt-gh-pages');
+
+  grunt.registerTask('default', ['gh-pages']);
+
+};
